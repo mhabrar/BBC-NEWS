5c2c4cdda312889c8aa8d062aa696142a1eef681
mhabrar <mdabrar15009@gmail.com>
2022-10-31 14:37:57 +0600

    bbc news

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens:{
+      sm:'440px',
+      md:'768px',
+      lg:'976px',
+      xl:'1440px'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
